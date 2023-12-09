b1d9fd79bba93ad45d3eac52604a057d54a955d7
elyssahancock <elyssabh@byui.edu>
2023-12-09 15:33:45 -0700

    functional

--- go.js
+++ go.js
@@ -1,1 +1,34 @@
-console.log("js is connected");
+// console.log("js is connected");
+// function getValue() {
+//     // Get the input element by its ID
+//     var inputElement = document.getElementById('num');
+
+//     // Or you can use document.querySelector
+//     // var inputElement = document.querySelector('#myInput');
+
+//     // Get the value of the input element
+//     var inputValue = inputElement.value;
+
+//     // Log the value to the console or do something with it
+//     console.log('Input value:', inputValue);
+// }
+// const ctx = document.getElementById('myChart');
+
+//   new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//       datasets: [{
+//         label: '# of Votes',
+//         data: [12, 19, 3, 5, 2, 3],
+//         borderWidth: 1
+//       }]
+//     },
+//     options: {
+//       scales: {
+//         y: {
+//           beginAtZero: true
+//         }
+//       }
+//     }
+//   });
